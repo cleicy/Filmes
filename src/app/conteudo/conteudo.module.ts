52d665c206fb40e16e2d0fa955e10f6bda479984
Cleicy Guião <cle.mad@hotmail.com>
2018-04-23 16:52:37 -0300

    =termino

--- src/app/conteudo/conteudo.module.ts
+++ src/app/conteudo/conteudo.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { FilmeCardComponent } from './filme-card/filme-card.component';
 import { FilmeService } from './filme.service';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormPesquisaComponent } from './lista-filmes/form-pesquisa/form-pesquisa.component'
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  declarations: [ListaFilmesComponent, FilmeCardComponent],
+  declarations: [ListaFilmesComponent, FilmeCardComponent, FormPesquisaComponent],
   exports: [
     ListaFilmesComponent,
     FilmeCardComponent
